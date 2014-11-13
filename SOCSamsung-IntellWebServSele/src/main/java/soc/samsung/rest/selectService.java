c100e36650f4381f5582a20e6fb33b39c0e0ea2f
Gautam Madaan <gautammadaan@gautam-mac.sv.cmu.local>
2014-11-13 15:35:50 -0800

    Project Creation
    Change-Id: I8cc35ff3ba6c10eebb9d9d92a9084b5ded61f482
    Signed-off-by: Gautam Madaan <gautammadaan@gautam-mac.sv.cmu.local>

---
+++ SOCSamsung-IntellWebServSele/src/main/java/soc/samsung/rest/selectService.java
@@ -0,0 +1,10 @@
+package soc.samsung.rest;
+
+public class selectService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
